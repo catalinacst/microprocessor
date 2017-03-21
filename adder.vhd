6acde4750f295859f6ac1a4057ff510f0ddcf5f7
catalinacst <catalina.cst@utp.edu.co>
2017-03-21 17:47:07 -0500

    Agregué programCounter y adder.

---
+++ adder.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity adder is
+    Port ( op1 : in  STD_LOGIC_VECTOR (31 downto 0);
+           op2 : in  STD_LOGIC_VECTOR (31 downto 0);
+           result : out  STD_LOGIC_VECTOR (31 downto 0));
+end adder;
+
+architecture Behavioral of adder is
+
+begin
+
+result <= op1 + op2;
+
+end Behavioral;
+
---
+++ programCounter.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity programCounter is
+    Port ( data : in  STD_LOGIC_VECTOR (31 downto 0);
+           rst : in  STD_LOGIC;
+           clk : in  STD_LOGIC;
+           dataOut : out  STD_LOGIC_VECTOR (31 downto 0));
+end programCounter;
+
+architecture Behavioral of programCounter is
+
+begin
+process (clk, rst)
+begin
+	if(rising_edge(clk)) then
+		if (rst = '1') then
+			dataOut <= "00000000000000000000000000000000";
+		else
+			dataOut <= data;
+		end if;
+	end if;
+end process;
+end Behavioral;
+
